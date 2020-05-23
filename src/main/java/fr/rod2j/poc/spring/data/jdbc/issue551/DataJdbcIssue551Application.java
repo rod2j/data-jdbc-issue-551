5ef36aa7af9d5a0f3111b2522f45338b45b837b2
Rodolphe LECOCQ <rodolphe.lecocq@soprasteria.com>
2020-05-23 18:07:14 +0200

    fix: remove starter-web, add @EnableJdbcRepositories

--- src/main/java/fr/rod2j/poc/spring/data/jdbc/issue551/DataJdbcIssue551Application.java
+++ src/main/java/fr/rod2j/poc/spring/data/jdbc/issue551/DataJdbcIssue551Application.java
@@ -2,8 +2,10 @@ package fr.rod2j.poc.spring.data.jdbc.issue551;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories
 public class DataJdbcIssue551Application {
 
 	public static void main(String[] args) {
