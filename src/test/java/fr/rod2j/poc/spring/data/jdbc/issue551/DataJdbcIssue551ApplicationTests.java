405e64a075f58c717190645b64906edf366197b9
Rodolphe LECOCQ <rodolphe.lecocq@soprasteria.com>
2020-05-26 23:41:45 +0200

    added test for actual deletion when return non void - bad luck

--- src/test/java/fr/rod2j/poc/spring/data/jdbc/issue551/DataJdbcIssue551ApplicationTests.java
+++ src/test/java/fr/rod2j/poc/spring/data/jdbc/issue551/DataJdbcIssue551ApplicationTests.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,8 +19,13 @@ class DataJdbcIssue551ApplicationTests {
 	void contextLoads() {
 	}
 
+	@BeforeEach
+	void beforeEach() {
+		repo.deleteAll();
+	}
+	
 	@Test
-	void canCreateAndDeleteWithListReturnType() {
+	void canDeleteWithListReturnType() {
 		City city1 = repo.save(City.of("Grenoble", "FR"));
 		assertThat(city1.getId()).isNotNull();
 		City retrieved = repo.findById(city1.getId()).get();
@@ -30,9 +36,22 @@ class DataJdbcIssue551ApplicationTests {
 		
 		List<City> deletedCAList = repo.removeByNameAndCountryCode("Vancouver", "CA");
 		assertThat(deletedCAList).size().isEqualTo(1);
+		assertThat(repo.findById(city2.getId())).isEmpty(); // fails
 		
-		long nbDeletedFR = repo.deleteByNameAndCountryCode("Grenoble", "FR");
+	}
+	
+	@Test
+	void canDeleteWithLongReturnType() {
+		City city1 = repo.save(City.of("Grenoble", "FR"));
+		assertThat(city1.getId()).isNotNull();
+		City retrieved = repo.findById(city1.getId()).get();
+		assertThat(retrieved).isEqualTo(city1);
+
+		City city2 = repo.save(City.of("Vancouver", "CA"));
+		assertThat(city2.getId()).isNotNull();
+		
+		long nbDeletedFR = repo.deleteByNameAndCountryCode("Grenoble", "FR"); // fails
 		assertThat(nbDeletedFR).isEqualTo(1);
-		
+		assertThat(repo.findById(city1.getId())).isEmpty();
 	}
 }
